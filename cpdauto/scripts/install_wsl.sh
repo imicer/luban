962e189ec928526cc15d8f21f4b15e55812ff808
hongweijia <jia-hongwei@126.com>
2021-10-23 20:45:47 +0800

    Update install_wsl.sh

--- cpdauto/scripts/install_wsl.sh
+++ cpdauto/scripts/install_wsl.sh
@@ -11,31 +11,55 @@ CPD_LICENSE=$6
 STORAGE_CLASS=$7
 
 # # Clone yaml files from the templates
-unalias cp
+if [[ $(type -t cp) == "alias" ]]
+then
+  unalias cp
+  echo "unalias cp completed."
+fi
 cp ./templates/cpd/wsl-sub.yaml wsl-sub.yaml
+cp ./templates/cpd/wsl-cr.yaml wsl-cr.yaml
+
+mkdir -p ./logs
+touch ./logs/install_wsl.log
+echo '' > ./logs/install_wsl.log
+
+# Create wsl catalog source 
+
+echo '*** executing **** create WSL catalog source' >> ./logs/install_cpd_platform.log
+
+cloudctl case launch \
+  --case ${OFFLINEDIR}/${CASE_PACKAGE_NAME} \
+  --inventory wslSetup \
+  --namespace openshift-marketplace \
+  --action install-catalog \
+  --args "--registry ${PRIVATE_REGISTRY} --inputDir ${OFFLINEDIR} --recursive"
+
+
+sleep 1m
+
 # Install wsl operator 
 
-sed -i -e s#OPERATOR_NAMESPACE#${OP_NAMESPACE}#g wsl-sub.yaml
+sed -i -e s#CPD_OPERATORS_NAMESPACE#${CPD_OPERATORS_NAMESPACE}#g wsl-sub.yaml
 
-echo '*** executing **** oc create -f wsl-sub.yaml'
-result=$(oc create -f wsl-sub.yaml)
-echo $result
+echo '*** executing **** oc apply -f wsl-sub.yaml' >> ./logs/install_wsl.log
+result=$(oc apply -f wsl-sub.yaml)
+echo $result  >> ./logs/install_wsl.log
 sleep 1m
 
 
 # Checking if the wsl operator pods are ready and running. 
 
-./pod-status-check.sh ibm-cpd-ws-operator ${OP_NAMESPACE}
+./pod-status-check.sh ibm-cpd-ws-operator ${CPD_OPERATORS_NAMESPACE}
 
 # switch zen namespace
 
-oc project ${NAMESPACE}
+oc project ${CPD_INSTANCE_NAMESPACE}
 
 # Create wsl CR: 
-sed -i -e s#CPD_NAMESPACE#${NAMESPACE}#g wsl-cr.yaml
-result=$(oc create -f wsl-cr.yaml)
-echo $result
+sed -i -e s#CPD_INSTANCE_NAMESPACE#${CPD_INSTANCE_NAMESPACE}#g wsl-cr.yaml
+result=$(oc apply -f wsl-cr.yaml)
+echo $result >> ./logs/install_wsl.log
 
 # check the WSL cr status
 
-./check-cr-status.sh ws ws-cr ${NAMESPACE} wsStatus
+./check-cr-status.sh ws ws-cr ${CPD_INSTANCE_NAMESPACE} wsStatus
