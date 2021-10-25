256f4b1c6020c526495bc4fe317159b45c3f6f3b
hongweijia <jia-hongwei@126.com>
2021-10-25 21:53:07 +0800

    Update setup-img-content-source-policy.sh

--- cpdauto/scripts/setup-img-content-source-policy.sh
+++ cpdauto/scripts/setup-img-content-source-policy.sh
@@ -3,7 +3,8 @@
 IMAGE_REGISTRY_URL=$1
 
 # # Clone yaml files from the templates
-if [[ $(type -t cp) == "alias" ]] then
+if [[ $(type -t cp) == "alias" ]]
+then
   unalias cp
   echo "unalias cp completed."
 fi
@@ -13,11 +14,12 @@ sed -i -e s#PRIVATE_REGISTRY#${IMAGE_REGISTRY_URL}#g image_content_source_policy
 
 mkdir -p ./logs
 touch ./logs/setup-img-content-source-policy.log
+echo '' > ./logs/setup-img-content-source-policy.log
 
 echo '*** delete existing imagecontentsourcepolicy cloud-pak-for-data-mirror' >> ./logs/setup-img-content-source-policy.log
 for p in $(oc get imagecontentsourcepolicy | grep cloud-pak-for-data-mirror| awk '{print $1}') ; do oc delete imagecontentsourcepolicy $p; done
 
 echo '*** executing **** oc apply -f image_content_source_policy.yaml' >> ./logs/setup-img-content-source-policy.log
 result=$(oc apply -f image_content_source_policy.yaml)
-echo $result ./logs/setup-img-content-source-policy.log
+echo $result >> ./logs/setup-img-content-source-policy.log
 sleep 15m
